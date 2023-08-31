decc22b9dd75513ab872da13b6c72e0045e52716
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-08-30 22:58:53 -0300

    adjustment on review

--- all.js
+++ all.js
@@ -1,14 +1,5 @@
 function showLoading(element) {
-    const messageCard = document.getElementById('messageCard');
-    element.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Carregando...';
-
-    setTimeout(function () {
-        element.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Carregando...';
-        messageCard.classList.remove('d-none');
-
-        setTimeout(function () {
-            element.innerHTML = '<i class="fas fa-exclamation-circle"></i> Sistema indisponível';
-        }, 3000);
-    }, 2000); 
+    element.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Aguarde...';
 }
+
 
