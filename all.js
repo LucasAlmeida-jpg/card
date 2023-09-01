34c9c1dcc95f2fd42c156ece871aa4f561ad9812
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-09-01 14:57:34 -0300

    new briefing adjustment

--- all.js
+++ all.js
@@ -3,3 +3,20 @@ function showLoading(element) {
 }
 
 
+function atualizarHorario() {
+    // Obtém a hora atual
+    const dataAtual = new Date();
+    const hora = dataAtual.getHours();
+    const minutos = dataAtual.getMinutes();
+    const segundos = dataAtual.getSeconds();
+
+    // Formata a hora como "hh:mm:ss"
+    const horaFormatada = `${hora}:${minutos}:${segundos}`;
+
+    // Atualiza o conteúdo da tag <span>
+    document.getElementById("hora-atual").textContent = horaFormatada;
+}
+
+atualizarHorario();
+
+setInterval(atualizarHorario, 1000);
